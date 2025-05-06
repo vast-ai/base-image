9b1316b56e6bbe52154728b0f1f89fec8beee3d3
Rob Ballantyne <rob@robballantyne.co.uk>
2025-05-06 15:45:51 +0100

    Add vars to enable pulling from fork or using alternative commit hash

--- derivatives/pytorch/provisioning_scripts/dia_1.6b.sh
+++ derivatives/pytorch/provisioning_scripts/dia_1.6b.sh
@@ -1,16 +1,51 @@
-#!/bin/bash 
+#!/bin/bash
+
+# Provisioning should not complete if there is an error. Retry on reboot if it failed
 set -eou pipefail
 
+# Ensure downloaded models are in the workspace
+export HF_HOME="${DATA_DIRECTORY}/huggingface"
+
+# Allow user to fetch alternative repo, branch, tag, commit
+export APP_REPO_URL=${APP_REPO_URL:-https://github.com/nari-labs/dia}
+export APP_REF=${APP_REF:-main}
+export MODEL=${MODEL:-nari-labs/Dia-1.6B}
+
+# Install the software into the default venv
 . /venv/main/bin/activate
-
-cd ${DATA_DIRECTORY}
-
-git clone https://github.com/nari-labs/dia.git
-
-cd dia
-
+cd "${DATA_DIRECTORY}"
+git clone "$APP_REPO_URL"
+cd $(basename "$APP_REPO_URL")
+git checkout "$APP_REF"
 pip install -e .
 
+# Download the models.  Not strictly necessary because app.py handles this but gives user more insight into the startup and allows retry on failure
+MAX_ATTEMPTS=5
+DELAY_SECONDS=10
+ATTEMPT=1
+echo "Downloading $MODEL from HuggingFace..."
+while [ $ATTEMPT -le $MAX_ATTEMPTS ]
+do
+    echo "Attempt $ATTEMPT of $MAX_ATTEMPTS..."
+    
+    huggingface-cli download "$MODEL" --include "*.json" "*.safetensors" 2>&1
+    
+    if [ $? -eq 0 ]; then
+        echo "Download completed successfully!"
+        break
+    else
+        echo "Attempt $ATTEMPT failed. Waiting $DELAY_SECONDS seconds before retrying..."
+        sleep $DELAY_SECONDS
+        ATTEMPT=$((ATTEMPT+1))
+    fi
+done
+
+if [ $ATTEMPT -gt $MAX_ATTEMPTS ]; then
+    echo "WARNING: Maximum attempts reached. Download of $MODEL failed.  You will need to manually download the model."
+fi
+
+
+# Generate the launch script for supervisord
 cat > /opt/supervisor-scripts/dia.sh << 'EOL'
 #!/bin/bash
 
@@ -39,18 +74,24 @@ while [ -f "/.provisioning" ]; do
 done
 
 cd ${DATA_DIRECTORY}/dia
+export GRADIO_SERVER_NAME=${GRADIO_SERVER_NAME:-127.0.0.1}
+export GRADIO_SERVER_PORT=${GRADIO_SERVER_PORT:-17860}
+
+echo "Starting Nari-Labs Dia application.  UI will be available when the models have been fetched and loaded." | tee -a "/var/log/portal/${PROC_NAME}.log"
+
 python app.py 2>&1 | tee -a "/var/log/portal/${PROC_NAME}.log"
 
 EOL
 
 chmod +x /opt/supervisor-scripts/dia.sh
 
+# Generate the supervisor config file
 cat > /etc/supervisor/conf.d/dia.conf << 'EOL'
 [program:dia]
 environment=PROC_NAME="%(program_name)s"
 command=/opt/supervisor-scripts/dia.sh
 autostart=true
-autorestart=unexpected
+autorestart=true
 exitcodes=0
 startsecs=0
 stopasgroup=true
@@ -65,5 +106,6 @@ stdout_logfile_maxbytes=0
 stdout_logfile_backups=0
 EOL
 
+# Update supervisor to start the new service
 supervisorctl reread
 supervisorctl update
