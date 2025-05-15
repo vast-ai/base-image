aec1c285a96edaaa2145c78d14f9c0fcb55d5099
Rob Ballantyne <rob@robballantyne.co.uk>
2025-05-15 17:26:57 +0100

    path fix

--- derivatives/linux-desktop/provisioning_scripts/tari-universe.sh
+++ derivatives/linux-desktop/provisioning_scripts/tari-universe.sh
@@ -10,15 +10,11 @@ chown $USER_NAME:$USER_NAME /workspace/tari-universe
 # Execute the rest of the commands as the specified user
 sudo -u $USER_NAME bash << 'USERSCRIPT'
 cd /workspace/tari-universe
-
 TARI_VERSION=$(curl -s https://api.github.com/repos/tari-project/universe/releases | grep -m 1 "tag_name" | cut -d'"' -f4)
 VERSION_NUMBER=${TARI_VERSION#v}
 FILE_NAME="tari_universe_${VERSION_NUMBER}_amd64.AppImage"
-
 wget -O ${FILE_NAME} https://github.com/tari-project/universe/releases/download/${TARI_VERSION}/${FILE_NAME}
-
 chmod +x $FILE_NAME
-
 ./"$FILE_NAME" --appimage-extract
 USERSCRIPT
 
@@ -29,8 +25,8 @@ Version=1.0
 Type=Application
 Name=Tari
 Comment=Tari Application
-Exec=/workspace/tari/squashfs-root/AppRun
-Icon=/workspace/tari/squashfs-root/tari_universe.png
+Exec=/workspace/tari-universe/squashfs-root/AppRun
+Icon=/workspace/tari-universe/squashfs-root/tari_universe.png
 Terminal=false
 Categories=Utility;
 StartupNotify=true
