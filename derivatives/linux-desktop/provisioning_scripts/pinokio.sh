dd2421dd0b807bf92874659d9082d72df7172121
Rob Ballantyne <rob@robballantyne.co.uk>
2025-05-20 13:45:33 +0100

    Improved readability and error handling

--- derivatives/linux-desktop/provisioning_scripts/pinokio.sh
+++ derivatives/linux-desktop/provisioning_scripts/pinokio.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-USER_NAME="user"
-USER_HOME="/home/$USER_NAME"
+set -euo pipefail
+
+export USER_NAME="user"
+export USER_HOME="/home/${USER_NAME}"
 
 # Execute the rest of the commands as the specified user
 bash << 'EOF'
+set -euo pipefail
 cd /tmp
-[[ -n $PINOKIO_VERSION ]] || PINOKIO_VERSION=$(curl -s https://api.github.com/repos/pinokiocomputer/pinokio/releases/latest |jq -r .tag_name)
-VERSION_NUMBER=${PINOKIO_VERSION#v}
+[[ -n ${PINOKIO_VERSION:-} ]] || PINOKIO_VERSION=$(curl -s https://api.github.com/repos/pinokiocomputer/pinokio/releases/latest |jq -r .tag_name)
+VERSION_NUMBER=${PINOKIO_VERSION}
 FILE_NAME="pinokio_${VERSION_NUMBER}_amd64.AppImage"
-wget -O ${FILE_NAME} https://github.com/pinokiocomputer/pinokio/releases/download/${VERSION_NUMBER}/Pinokio-${VERSION_NUMBER}.AppImage
-chmod +x $FILE_NAME
-./"$FILE_NAME" --appimage-extract
+wget -O "${FILE_NAME}" "https://github.com/pinokiocomputer/pinokio/releases/download/${VERSION_NUMBER}/Pinokio-${VERSION_NUMBER}.AppImage"
+chmod +x "${FILE_NAME}"
+./"${FILE_NAME}" --appimage-extract
 mv /tmp/squashfs-root /opt/pinokio
-chown -R $USER_NAME:$USER_NAME /opt/pinokio
-rm -f /tmp/pinokio*
+chown -R "${USER_NAME}:${USER_NAME}" /opt/pinokio
+rm -f "/tmp/${FILE_NAME}"
 EOF
 
 # Create the desktop file as the user
-sudo -u $USER_NAME bash -c "cat > $USER_HOME/Desktop/Pinokio.desktop << 'EOF'
+sudo -u ${USER_NAME} bash -c "cat > ${USER_HOME}/Desktop/Pinokio.desktop << 'EOF'
 [Desktop Entry]
 Version=1.0
 Type=Application
 Name=Pinokio
 Comment=Pinokio Application
-Exec=env HOME=$WORKSPACE APPDIR=/opt/pinokio /opt/pinokio/AppRun --no-sandbox %U
+Exec=env HOME=${WORKSPACE} APPDIR=/opt/pinokio /opt/pinokio/AppRun --no-sandbox %U
 Icon=/opt/pinokio/pinokio.png
 Terminal=false
 Categories=Utility;
@@ -32,4 +35,4 @@ StartupNotify=true
 EOF"
 
 # Make the desktop file executable as the user
-sudo -u $USER_NAME chmod +x $USER_HOME/Desktop/Pinokio.desktop
+sudo -u "${USER_NAME}" chmod +x "${USER_HOME}/Desktop/Pinokio.desktop"
