951759ed3322417b47d2a64a84da47d7afb8a411
Rob Ballantyne <rob@robballantyne.co.uk>
2025-09-26 16:17:57 +0100

    Auto updates, API wrapper

---
+++ derivatives/pytorch/derivatives/comfyui/ROOT/opt/supervisor-scripts/api-wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+utils=/opt/supervisor-scripts/utils
+. "${utils}/logging.sh"
+. "${utils}/cleanup_generic.sh"
+. "${utils}/environment.sh"
+. "${utils}/exit_portal.sh" "API Wrapper"
+
+# Wait for provisioning to complete
+while [ -f "/.provisioning" ]; do
+    echo "$PROC_NAME startup paused until instance provisioning has completed (/.provisioning present)"
+    sleep 5
+done
+
+# Launch ComfyUI API Wrapper
+cd /opt/comfyui-api-wrapper
+. .venv/bin/activate
+
+uvicorn main:app --port 18288 2>&1
--- derivatives/pytorch/derivatives/comfyui/ROOT/opt/supervisor-scripts/comfyui.sh
+++ derivatives/pytorch/derivatives/comfyui/ROOT/opt/supervisor-scripts/comfyui.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-# User can configure startup by removing the reference in /etc.portal.yaml - So wait for that file and check it
-while [ ! -f "$(realpath -q /etc/portal.yaml 2>/dev/null)" ]; do
-    echo "Waiting for /etc/portal.yaml before starting ${PROC_NAME}..." | tee -a "/var/log/portal/${PROC_NAME}.log"
-    sleep 1
-done
-
-# Check for comfyui in the portal config
-search_term="comfyui"
-search_pattern=$(echo "$search_term" | sed 's/[ _-]/[ _-]/g')
-if ! grep -qiE "^[^#].*${search_pattern}" /etc/portal.yaml; then
-    echo "Skipping startup for ${PROC_NAME} (not in /etc/portal.yaml)" | tee -a "/var/log/portal/${PROC_NAME}.log"
-    exit 0
-fi
+utils=/opt/supervisor-scripts/utils
+. "${utils}/logging.sh"
+. "${utils}/cleanup_generic.sh"
+. "${utils}/environment.sh"
+. "${utils}/exit_portal.sh" "ComfyUI"
 
 # Activate the venv
 . /venv/main/bin/activate
 
-# Wait for provisioning to complete
+COMFYUI_DIR=${WORKSPACE}/ComfyUI
 
+# Not first boot - Do this to handle frontend being out of sync after manager update
+if [[ ! -f /.provisioning ]]; then
+    cd "${COMFYUI_DIR}"
+    uv pip --no-cache-dir install -r requirements.txt
+fi
+
+# Wait for provisioning to complete
 while [ -f "/.provisioning" ]; do
     echo "$PROC_NAME startup paused until instance provisioning has completed (/.provisioning present)"
-    sleep 10
+    sleep 5
 done
 
-# Avoid git errors because we run as root but files are owned by 'user'
-export GIT_CONFIG_GLOBAL=/tmp/temporary-git-config
-git config --file $GIT_CONFIG_GLOBAL --add safe.directory '*'
-
 # Launch ComfyUI
-cd ${WORKSPACE}/ComfyUI
+cd "${COMFYUI_DIR}"
 LD_PRELOAD=libtcmalloc_minimal.so.4 \
         python main.py \
-        ${COMFYUI_ARGS:---disable-auto-launch --port 18188 --enable-cors-header} 2>&1 | tee -a "/var/log/portal/${PROC_NAME}.log"
+        ${COMFYUI_ARGS:---disable-auto-launch --port 18188 --enable-cors-header} 2>&1
+
