0e9db3f7e76314e3b1f21f6e68bd387ecfc09667
Rob Ballantyne <rob@robballantyne.co.uk>
2025-10-13 14:26:48 +0100

    Improve variable checks

--- derivatives/pytorch/derivatives/vllm/provisioning_scripts/default.sh
+++ derivatives/pytorch/derivatives/vllm/provisioning_scripts/default.sh
@@ -4,12 +4,13 @@ set -euo pipefail
 . /venv/main/bin/activate
 
 # Add additional python packages if declared
-if [[ -n "${PIP_INSTALL}" ]]; then
+if [[ -n "${PIP_INSTALL:-}" ]]; then
     uv pip install ${PIP_INSTALL}
 fi
 
 # Install Deep GEMM if specified - Required for Deepseek sparse attn
-if [[ "${INSTALL_DEEPGEMM,,}" = "true" ]]; then
+install_deepgemm=${INSTALL_DEEPGEMM:-false}
+if [[ "${install_deepgemm,,}" = "true" ]]; then
     cd /tmp
     git clone --recursive https://github.com/deepseek-ai/DeepGEMM.git
     cd DeepGEMM
