1317a10c681a44540d1fe038c846819673761934
Rob Ballantyne <rob@vast.ai>
2025-10-28 13:48:25 +0000

    Adds script for wan2GP install

---
+++ provisioning_scripts/wan2gp.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -eou pipefail
+
+. /venv/main/bin/activate
+
+apt-get install -y \
+    libasound2-dev \
+    pulseaudio-utils \
+    --no-install-recommends
+
+cd "$WORKSPACE"
+[[ -d "${WORKSPACE}/Wan2GP" ]] || git clone https://github.com/deepbeepmeep/Wan2GP
+cd Wan2GP
+[[ -n "{WAN2GP_VERSION:-}" ]] && git checkout "$WAN2GP_VERSION"
+
+uv pip install torch==${TORCH_VERSION:-2.8.0} torchvision torchaudio --torch-backend=auto
+uv pip install -r requirements.txt
+
+# Create Wan2GP startup scripts
+cat > /opt/supervisor-scripts/wan2gp.sh << 'EOL'
+#!/bin/bash
+
+utils=/opt/supervisor-scripts/utils
+. "${utils}/logging.sh"
+. "${utils}/cleanup_generic.sh"
+. "${utils}/environment.sh"
+. "${utils}/exit_serverless.sh"
+. "${utils}/exit_portal.sh" "Wan2GP"
+
+echo "Starting Wan2GP"
+
+. /etc/environment
+. /venv/main/bin/activate
+
+cd "${WORKSPACE}/Wan2GP"
+export XDG_RUNTIME_DIR=/tmp
+export SDL_AUDIODRIVER=dummy
+python wgp.py 2>&1
+
+EOL
+
+chmod +x /opt/supervisor-scripts/wan2gp.sh
+
+# Generate the supervisor config files
+cat > /etc/supervisor/conf.d/wan2gp.conf << 'EOL'
+[program:wan2gp]
+environment=PROC_NAME="%(program_name)s"
+command=/opt/supervisor-scripts/wan2gp.sh
+autostart=true
+autorestart=true
+exitcodes=0
+startsecs=0
+stopasgroup=true
+killasgroup=true
+stopsignal=TERM
+stopwaitsecs=10
+# This is necessary for Vast logging to work alongside the Portal logs (Must output to /dev/stdout)
+stdout_logfile=/dev/stdout
+redirect_stderr=true
+stdout_events_enabled=true
+stdout_logfile_maxbytes=0
+stdout_logfile_backups=0
+EOL
+
+# Update supervisor to start the new service
+supervisorctl reread
+supervisorctl update
