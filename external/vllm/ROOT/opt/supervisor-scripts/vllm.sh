dbe25d26284deeaafc4ff9c5ec611ab0f871b898
Rob Ballantyne <rob@vast.ai>
2026-04-10 17:25:33 +0100

    Stop setting DP when EP enabled

--- external/vllm/ROOT/opt/supervisor-scripts/vllm.sh
+++ external/vllm/ROOT/opt/supervisor-scripts/vllm.sh
@@ -52,14 +52,10 @@ fi
 ## Automatically use all GPUs
 AUTO_PARALLEL_ARGS=""
 # Rewrite var name
-AUTO_PARALLEL="${AUTO_PARALLEL:-true}"
-if [[ "${AUTO_PARALLEL,,}" = "true" ]] && ! [[ $VLLM_ARGS =~ tensor-parallel-size || $VLLM_ARGS =~ data-parallel-size ]]; then
-    if [[ $VLLM_ARGS =~ enable-expert-parallel ]]; then
-        AUTO_PARALLEL_ARGS="--tensor-parallel-size 1 --data-parallel-size $GPU_COUNT"
-    else
-        AUTO_PARALLEL_ARGS="--tensor-parallel-size $GPU_COUNT"
-    fi
-fi
+AUTO_PARALLEL="${AUTO_PARALLEL:-true}"                                                                           
+  if [[ "${AUTO_PARALLEL,,}" = "true" ]] && ! [[ $VLLM_ARGS =~ tensor-parallel-size || $VLLM_ARGS =~ data-parallel-size ]]; then                                                                                      
+      AUTO_PARALLEL_ARGS="--tensor-parallel-size $GPU_COUNT"                                                       
+  fi 
 
 # Read complex args from /etc/vllm-args.conf if env vars were unsuitable
 eval "pty vllm serve "${VLLM_MODEL:-}" ${VLLM_ARGS:-} ${AUTO_PARALLEL_ARGS} $([[ -f /etc/vllm-args.conf ]] && cat /etc/vllm-args.conf)" 2>&1
